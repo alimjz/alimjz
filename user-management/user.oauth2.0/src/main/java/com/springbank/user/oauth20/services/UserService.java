97b98d765160e1082dc0ab43047e9c383e863641
Ali <alimjz@users.noreply.github.com>
2022-08-12 23:08:38 +0430

    Initial

---
+++ user-management/user.oauth2.0/src/main/java/com/springbank/user/oauth20/services/UserService.java
@@ -0,0 +1,40 @@
+package com.springbank.user.oauth20.services;
+
+import com.springbank.user.oauth20.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service(value = "userService")
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+
+        System.out.println(user.toString());
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("Incorrect Username / Password supplied!");
+        }
+
+        var account = user.get(0).getAccount();
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(username)
+                .password(account.getPassword())
+                .authorities(account.getRoles())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
